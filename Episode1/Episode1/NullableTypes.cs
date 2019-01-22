9f114eb5d12195308b0f68a99f166c352ea61ab6
Chuck Conway <charles@cconway.com>
2019-01-22 15:21:44 -0800

    first commit

---
+++ Episode1/Episode1/NullableTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Episode1
+{
+    public class NullableTypes
+    {
+        //public int ConvertToInt(decimal? kids)
+        //{
+
+        //}
+
+        public decimal ConvertToDecimal(decimal? kids)
+        {
+            return (decimal) kids;
+        }
+    }
+}
---
+++ Episode1/Episode1/ValueTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Episode1
+{
+    public class ValueTypes
+    {
+        public int ConvertToInt(decimal kids)
+        {
+            return (int) kids;
+        }
+    }
+}
