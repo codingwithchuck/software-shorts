dce410e42b5be85a4acf8f959f5b5d703567b46b
Chuck Conway <charles@cconway.com>
2019-01-22 15:24:57 -0800

    Removed comments.

--- Episode1/Episode1/NullableTypes.cs
+++ Episode1/Episode1/NullableTypes.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Episode1
 {
     public class NullableTypes
     {
-        //public int ConvertToInt(decimal? kids)
-        //{
-
-        //}
-
         public decimal ConvertToDecimal(decimal? kids)
         {
             return (decimal) kids;
