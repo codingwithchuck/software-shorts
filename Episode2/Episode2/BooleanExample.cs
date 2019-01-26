597d565d87783941f7b4257b7d0e7c293233f376
Chuck Conway <charles@cconway.com>
2019-01-26 10:54:42 -0800

    Episode 2 code.

---
+++ Episode2/Episode2/BooleanExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Episode2
+{
+    public class BooleanExample
+    {
+        public string Check(bool hasValue)
+        {
+            if (!hasValue)
+            {
+                return GetChuck();
+            }
+
+            return GetErin();
+        }
+
+        private string GetChuck()
+        {
+            return "Chuck";
+        }
+
+        private string GetErin()
+        {
+            return "Erin";
+        }
+    }
+}
+ 
---
+++ Episode2/Episode2/NullableBooleanExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.Metadata;
+
+namespace Episode2
+{
+    public class NullableBooleanExample
+    {
+        public string GetValue(bool? hasItems = null)
+        {
+            if (hasItems == true)
+            {
+                return GetChuck();
+            }
+
+            return GetErin();
+        }
+
+        private string GetChuck()
+        {
+            return "Chuck";
+        }
+
+        private string GetErin()
+        {
+            return "Erin";
+        }
+    }
+}
